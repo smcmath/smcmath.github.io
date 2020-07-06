e933e6a8c7a79ebf8d212770b311a1f50d5e2bdc
Anthony Wang <40553084+cosmicgenius@users.noreply.github.com>
2020-07-06 14:15:19 -0700

    Fix down arrow display

--- assets/js/overlayScrollbars.js
+++ assets/js/overlayScrollbars.js
@@ -1,6 +1,22 @@
 (function($) {
   "use strict";
-  $('body').overlayScrollbars({
+  $("body").overlayScrollbars({
     className: "os-theme-dark os-theme-dark-edgy",
+    callbacks : {
+      /* Deal with down arrow display based on height scrolled */
+      onScroll: function() {
+        if (this != null) {
+          console.log(this.scroll().position.y);
+          var vw = getvSize()[0];
+          var vh = getvSize()[1];
+
+          if (this.scroll().position.y > (Math.min(vh - 92, 3 * vw / 4)/2)) {
+            $("#headerimage-down").fadeOut(500);
+          } else {
+            $("#headerimage-down").fadeIn(500);
+          }
+        }
+      }
+    }
   })
 })(jQuery)
